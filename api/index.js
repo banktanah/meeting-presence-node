0fa384186feb1b45c5ffa271fb362dd6a7e7f744
virtualynx <eryandi.angga.pratama@gmail.com>
2024-12-02 09:06:14 +0700

    set limit json and urlencoded param to 50mb

--- api/index.js
+++ api/index.js
@@ -4,8 +4,11 @@ const bodyParser = require('body-parser');
 const https = require('https');
 const axios = require('axios');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 app.use((req, res, next) => {
   // const allowedOrigins = [
@@ -20,7 +23,7 @@ app.use((req, res, next) => {
     allowedOrigins = allowedOrigins.map(a => a.trim());
   }
 
-  console.log('allowedOrigins', allowedOrigins);
+  // console.log('allowedOrigins', allowedOrigins);
 
   const origin = req.headers.origin;
   if(allowedOrigins.includes(origin)) {
